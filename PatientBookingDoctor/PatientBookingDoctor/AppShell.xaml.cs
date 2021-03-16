5dcda3565015f1ab7849f3bd11951a9520161ca3
tuanhienvu <tuanhienvu@gmail.com>
2021-03-16 11:30:19 +0700

    Add project files.

---
+++ PatientBookingDoctor/PatientBookingDoctor/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using PatientBookingDoctor.ViewModels;
+using PatientBookingDoctor.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace PatientBookingDoctor
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
